e233d40e7d5c3ebd34280096f72d76d3840e9754
Kyle Miller <millerkyle85@gmail.com>
2022-07-01 14:27:43 -0700

    add index method

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,20 @@
 class AnimalsController < ApplicationController
+  def index 
+    if params[:name].present?
+      name = params[:name]
+      @animals = Animal.search_name(name)
+    elsif params[:sex].present?
+      sex = params[:sex]
+      @animals = Animal.search_sex(sex)
+    elsif params[:breed].present?
+      breed = params[:breed]
+      @animals = Animal.search_breed(breed)
+    elsif paramas[:age].present?
+      age = paramas[:age]
+      @animals = Animal.search_age(age)
+    else 
+      @animals = Animal.order('name ASC').paginate(:page => params[:page], per_page:10)
+    end
+    json_response(@animals)
+  end
 end
