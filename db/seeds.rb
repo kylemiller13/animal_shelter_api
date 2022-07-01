d700115ed5c3a7ff1570c84aef2d2e2e9e2afe61
Kyle Miller <millerkyle85@gmail.com>
2022-07-01 14:29:24 -0700

    generate animal name,sex,breed,age with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Animal.destroy_all
+
+class Seed
+  def self.begin
+    seed = Seed.new
+    seed.generate_animals
+  end
+
+  def generate_animals
+    20.times do |i|
+      animal = Animal.create!(
+        name: Faker::Creature::Dog.name,
+        sex: Faker::Creature::Dog.gender,
+        breed: Faker::Creature::Dog.breed,
+        age: Faker::Number.between(from: 1, to: 30)
+      )
+      puts "Animal #{i}: This is #{animal.name}, sex is #{animal.sex}, breed is #{animal.breed}, and age is #{animal.age}."
+    end
+    20.times do |i|
+      animal = Animal.create!(
+        name: Faker::Creature::Cat.name,
+        sex: Faker::Creature::Dog.gender,
+        breed: Faker::Creature::Cat.breed,
+        age: Faker::Number.between(from: 1, to: 20)
+      )
+      puts "Animal #{i}: This is #{animal.name}, sex is #{animal.sex}, breed is #{animal.breed}, and age is #{animal.age}."
+    end
+  end
+end
+
+Seed.begin
