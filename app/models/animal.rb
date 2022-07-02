07211966dee8a166ec255407b221559ae8729b5b
Kyle Miller <millerkyle85@gmail.com>
2022-07-01 22:49:42 -0700

    add validates, scopes, and titleize method

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,18 @@
 class Animal < ApplicationRecord
+  validates :name, :presence => true 
+  validates :sex, :presence => true
+  validates :breed, :presence => true
+  validates :age, numericality: { only_integer: true }
+  scope :search_name, -> (name_parameter) { where("name ilike ?", "#{name_parameter}")}
+  scope :search_sex, -> (sex_parameter) { where("sex ilike ?", "#{sex_parameter}")}
+  scope :search_breed, -> (breed_parameter) { where("breed ilike ?", "#{breed_parameter}")}
+  scope :search_age, -> (age_parameter) { where("age = ?", "#{age_parameter}")}
+
+  before_save(:titleize_animal)
+
+  private
+
+  def titleize_animal
+    self.name = self.name.titleize
+  end
 end
