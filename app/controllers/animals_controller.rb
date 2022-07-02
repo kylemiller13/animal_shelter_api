5f7ad22bf7eaf6d23505055cb847baf373000b53
Kyle Miller <millerkyle85@gmail.com>
2022-07-01 18:46:45 -0700

    add create method

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -17,8 +17,15 @@ class AnimalsController < ApplicationController
     end
     json_response(@animals)
   end
+
   def show
     @animal = Animal.find(params[:id])
     json_response(@animal)
   end
+
+  def create
+    @animal = Animal.create!(animal_params)
+    json_response(@animal, :created)
+  end
+
 end
